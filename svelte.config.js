00ba6f4aa53049a0d0b37d6e0721ad51755c52f3
button9835 <diyoingson@naver.com>
2025-07-16 15:54:19 +0900

    2

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,16 @@
 import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
-};
+const dev = process.env.NODE_ENV === 'development';
 
-export default config;
+export default {
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    }),
+    paths: {
+      base: dev ? '' : '/K-Auth'
+    }
+  }
+};
