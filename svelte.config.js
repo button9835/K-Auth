27ba01a8bba0496f9b1338d03696183902cf5553
button9835 <diyoingson@naver.com>
2025-07-16 17:04:21 +0900

    11

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,6 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    appDir: 'app',
     adapter: adapter({
       pages: 'build',
       assets: 'build',
