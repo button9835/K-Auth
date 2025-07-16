b108b35a5c5265a2ddc1ba5051ed6694354f9284
button9835 <diyoingson@naver.com>
2025-07-16 16:45:35 +0900

    Deploy update

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from 'svelte-preprocess'; // ✅ 수정된 경로
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    // 앱 디렉토리 이름을 디폴트 "_app"이 아닌 다른 이름으로 변경
     appDir: 'app',
     adapter: adapter({
       pages: 'build',
